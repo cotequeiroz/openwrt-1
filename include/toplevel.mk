a03464acbe29757aab97c2743db733d8de8214c2
Eneas U de Queiroz <cotequeiroz@gmail.com>
2020-03-13 17:27:07 -0300

    build: make our config Makefile closer to upstream
    This is done in preparation for an update of scripts/config.
    
    scripts/config/Makefile has been patched in different ways so many times
    that it is hard to keep in sync with changes upstream.  This attempts to
    bring it, and other ancillary files, as close to upstream as possible.
    
    It includes a change in toplevel.mk to find pkg-config for make xconfig
    using the same strategy as the other config targets, i.e. by prepending
    the PATH variable with the location of the distro's pkg-config instead
    of passing it as a variable.
    
    Signed-off-by: Eneas U de Queiroz <cotequeiroz@gmail.com>

--- include/toplevel.mk
+++ include/toplevel.mk
@@ -101,6 +101,7 @@ prepare-tmpinfo: FORCE
 
 ifneq ($(DISTRO_PKG_CONFIG),)
 scripts/config/mconf: export PATH:=$(dir $(DISTRO_PKG_CONFIG)):$(PATH)
+scripts/config/qconf: export PATH:=$(dir $(DISTRO_PKG_CONFIG)):$(PATH)
 endif
 scripts/config/mconf:
 	@$(_SINGLE)$(SUBMAKE) -s -C scripts/config all CC="$(HOSTCC_WRAPPER)"
@@ -108,9 +109,8 @@ scripts/config/mconf:
 $(eval $(call rdep,scripts/config,scripts/config/mconf))
 
 scripts/config/qconf:
-	@$(_SINGLE)$(SUBMAKE) -s -C scripts/config qconf \
-		CC="$(HOSTCC_WRAPPER)" \
-		DISTRO-PKG-CONFIG="$(DISTRO_PKG_CONFIG)"
+	@$(_SINGLE)$(SUBMAKE) -s -C scripts/config conf qconf \
+		CC="$(HOSTCC_WRAPPER)"
 
 scripts/config/conf:
 	@$(_SINGLE)$(SUBMAKE) -s -C scripts/config conf CC="$(HOSTCC_WRAPPER)"
